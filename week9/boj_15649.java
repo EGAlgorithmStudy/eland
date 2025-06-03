b09670e2543102305e44075cb28be616f2579633
Elandland <68230815+Elandland@users.noreply.github.com>
2025-06-03 13:54:53 +0900

    feat. boj_15649 solve (백트래킹)

---
+++ week9/boj_15649.java
@@ -0,0 +1,51 @@
+package week9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15649 {
+
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+        arr= new int[M];
+        dfs(1,0);
+
+
+    }
+
+
+    public static void dfs(int n, int depth){
+
+        if(depth == M){
+            for(int i=0; i<M; i++){
+                System.out.print(arr[i]+ " ");
+            }
+            System.out.println("");
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[depth] = i+1;
+                dfs(n,depth+1);
+                visited[i] = false;
+            }
+        }
+
+
+    }
+}
